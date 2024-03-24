e5ccbbf76a471b552465781908fd0a95a57b71b0
ahmedhabeeb73 <alnaib888@gmail.com>
2024-03-24 13:04:58 +0100

    Setting the default site

--- app/Models/Site.php
+++ app/Models/Site.php
@@ -11,6 +11,15 @@ class Site extends Model
     protected $fillable = ['scheme', 'domain', 'default'];
     use HasFactory;
 
+    public static function booted()
+    {
+        static::updating(function (Site $site) {
+            if (in_array('default', array_keys($site->getDirty()))) {
+                $site->user->sites()->whereKeyNot($site->id)->update(['default' => false]);
+            }
+        });
+    }
+
 
     public function user()
     {
--- app/Models/User.php
+++ app/Models/User.php
@@ -3,9 +3,10 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Models\Site;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
@@ -43,5 +44,10 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function sites()
+    {
+        return $this->hasMany(Site::class);
     }
 }
